f44e7164ad993030e5f55b0e13de4121ddc962ad
hariharbajpai <harihar701764@gmail.com>
2024-04-14 18:20:41 +0530

    work in progress not completed ye

---
+++ Spotify/script.js
@@ -0,0 +1,22 @@
+console.log("lets go")
+
+async function getsongs(){
+
+    let a = await fetch("http://127.0.0.1:5500/github/Projects-Repo/Spotify/songs/")
+    let response = await a.text();
+    console.log(response);
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+
+    let songs = [];
+    for(let index = 0 ; index < as.length; index++){
+        const element = as[index];
+        if(element.href.endsWith(".mp3")){
+            songs.push(element.href);
+        }
+    }
+    return songs;
+}
+
+main();
